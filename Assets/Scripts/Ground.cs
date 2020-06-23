7ca5b70219eb83703f74cd8588dacea91a39b373
PIH <zasxcc@naver.com>
2020-06-23 12:35:18 +0900

    되려나

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("H=" + h.ToString());
+        Debug.Log("V=" + v.ToString());
+    }
+}
