24bcb9802a45cfb9a012f41c0a46259961405363
PIH <zasxcc@naver.com>
2020-06-30 17:04:19 +0900

    TitleScene 제작

---
+++ Assets/Scripts/TitleCharacterCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCharacterCtrl : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("Run", false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
