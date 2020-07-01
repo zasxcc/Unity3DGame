19da5a19695c82456772b95f030fcd1e946108f1
PIH <zasxcc@naver.com>
2020-07-01 13:16:01 +0900

    타이틀씬 GameStart, Highscore, Exit 버튼 스크립트 제작

---
+++ Assets/Scripts/HighScoreButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HighScoreButton : MonoBehaviour
+{
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = GetClickedObject();
+            
+            if (target.Equals(gameObject))
+            {
+                SceneManager.LoadScene("HighScoreScene");
+            }
+        }
+    }
+    private GameObject GetClickedObject()
+    {
+        RaycastHit hit;
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //마우스 포인트 근처 좌표를 만든다. 
+
+
+        if (true == (Physics.Raycast(ray.origin, ray.direction * 10, out hit)))   //마우스 근처에 오브젝트가 있는지 확인
+        {
+            //있으면 오브젝트를 저장한다.
+
+            target = hit.collider.gameObject;
+
+        }
+        return target;
+    }
+}
+
