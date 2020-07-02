b8dd883e2272d2ca6ad8ca997c729e5798bf5445
PIH <zasxcc@naver.com>
2020-07-03 01:40:01 +0900

    빌드를 위한 주석 처리

--- Assets/AllSkyFree/AllSkyFree_Menu.cs
+++ Assets/AllSkyFree/AllSkyFree_Menu.cs
@@ -7,11 +7,11 @@ namespace Com.AllSkyFree
 {
 	public class AllSkyFreeMenu : MonoBehaviour
 	{
-		[MenuItem("Window/AllSky/AllSky 200+ Skybox Set")]
-		static void Link()
-	    {
-		    Application.OpenURL( "https://assetstore.unity.com/packages/2d/textures-materials/sky/allsky-200-sky-skybox-set-10109" );
-	    }
+		//[MenuItem("Window/AllSky/AllSky 200+ Skybox Set")]
+		//static void Link()
+	 //   {
+		//    Application.OpenURL( "https://assetstore.unity.com/packages/2d/textures-materials/sky/allsky-200-sky-skybox-set-10109" );
+	 //   }
 	
 	}	
 }
